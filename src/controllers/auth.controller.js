0396cc64d45b97bc6964196e5b4c088769d31367
Navneet <navneetkumar.html@gmail.com>
2025-01-21 00:30:20 +0530

    remove case sensitivity from email auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,30 @@
 import { User } from '../models/user.model.js';
-
 export const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        // Find user by username
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-        // Compare hashed password
-        // const isPasswordValid = await bcrypt.compare(password, user.password);
-        // if (!isPasswordValid) {
-        //     return res.status(401).json({ message: 'Invalid credentials' });
-        // }
-         if(password == user.password){
-        // Successful login response
-        res.json({
-            message: 'Login successful',
-            user: {
-                id: user._id,
-                name: user.name,
-                role: user.role,
-                email: user.email,
-                phone: user.phone,
-                photo: user.photo,
-            },
-        });
+  const { email, password } = req.body;
+  try {
+    // Find user by email with case-insensitive search
+    const user = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    // Compare passwords
+    if (password === user.password) {
+      // Successful login response
+      res.json({
+        message: 'Login successful',
+        user: {
+          id: user._id,
+          name: user.name,
+          role: user.role,
+          email: user.email,
+          phone: user.phone,
+          photo: user.photo,
+        },
+      });
     } else {
-        return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
-    } // try block ends
-    catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
